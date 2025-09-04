b60012da1d88c1242605fc01853a848609856638
ValeRojasC <rcv0031261@est.univalle.edu>
2025-09-03 22:45:51 -0400

    cambionew

--- ultimo/generate_hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-// PHP: Generar hash para la contraseña "1234"
-$password = '1234';
-$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-// Imprimir el hash
-echo $hashedPassword;
-?>
