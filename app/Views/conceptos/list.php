ba72fe3c43de5d632d472cc296c6e2348c4a778b
Diego <rcd0030173@est.univalle.edu>
2025-09-19 00:51:24 -0400

    test

--- app/Views/conceptos/list.php
+++ app/Views/conceptos/list.php
@@ -348,6 +348,7 @@ ob_start();
         <option value="100" <?= $pageSize===100?'selected':'' ?>>100</option>
       </select>
     </form>
+    <h1>hola</h1>
 
     <div class="pagination-info">Página <?= $page ?> de <?= $totalPg ?> (<?= number_format($total) ?> registros)</div>
     
